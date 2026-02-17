64112900d9f4867777b1474e9468b19cd8666f44
QAmamomamo <hokayumi4@gmail.com>
2026-02-17 16:02:38 +0900

    モックモジュールを使ったスタブ

---
+++ src/04/02/greet3.test.ts
@@ -0,0 +1,15 @@
+import { greet, sayGoodBye } from "./greet";
+
+jest.mock("./greet", () => ({
+  ...jest.requireActual("./greet"),
+  sayGoodBye: (name: string) => `Good bye, ${name}.`,
+}));
+
+test("挨拶を返す (本来の実装通り)", () => {
+  expect(greet("taro")).toBe("Hello! taro")
+})
+
+test("さよならを返す (本来の実装ではない) ", () => {
+  const message = `${sayGoodBye("Taro")} See you.`;
+  expect(message).toBe("Good bye, Taro. See you.")
+})
