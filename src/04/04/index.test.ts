9f345b002aa9d8d1b7578589795bc51088c5b52e
QAmamomamo <hokayumi4@gmail.com>
2026-02-18 10:53:15 +0900

    モック生成関数

---
+++ src/04/04/index.test.ts
@@ -0,0 +1,47 @@
+import { getMyArticleLinksByCategory } from ".";
+import * as Fetchers from "../fetchers";
+import { getMyArticlesData, httpError } from "../fetchers/fixtures";
+
+jest.mock("../fetchers"); // import * as Fetchers from "../fetchers" は読み取り専用なのでモック化する必要があるっぽい
+
+function mockGetMyArticles(status = 200) {
+  if (status > 299) {
+    return jest
+      .spyOn(Fetchers, "getMyArticles")
+      .mockRejectedValueOnce(httpError);
+  }
+  return jest
+    .spyOn(Fetchers, "getMyArticles")
+    .mockResolvedValueOnce(getMyArticlesData);
+}
+
+test("指定したタグを持つ記事が一件もない場合、nullが返る", async () => {
+  mockGetMyArticles();
+  const data = await getMyArticleLinksByCategory("playwright");
+  expect(data).toBeNull();
+});
+
+test("指定したタグを持つ記事が一件以上ある場合、リンク一覧が返る", async () => {
+  mockGetMyArticles();
+  const data = await getMyArticleLinksByCategory("testing");
+  expect(data).toMatchObject([
+    {
+      link: "/articles/howto-testing-with-typescript",
+      title: "TypeScript を使ったテストの書き方",
+    },
+    {
+      link: "/articles/react-component-testing-with-jest",
+      title: "Jest ではじめる React のコンポーネントテスト",
+    },
+  ]);
+});
+
+
+test("データ取得に失敗した場合、rejectされる", async () => {
+  mockGetMyArticles(500);
+  await getMyArticleLinksByCategory("testing").catch((err) => {
+    expect(err).toMatchObject({
+      err: { message: "internal server error"}
+    })
+  })
+})
