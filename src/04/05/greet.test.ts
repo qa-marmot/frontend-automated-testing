8fedb8dc7df52dc4f0846ae8de291f2d28ec1f64
QAmamomamo <hokayumi4@gmail.com>
2026-02-22 18:25:38 +0900

    モック関数を使ったスパイ

---
+++ src/04/05/greet.test.ts
@@ -0,0 +1,64 @@
+import { greet } from "./greet";
+import { checkConfig } from "./checkConfig"
+
+test("モック関数は実行された", () => {
+    const mockFn = jest.fn();
+    mockFn();
+    expect(mockFn).toBeCalled();
+})
+
+test("モック関数は実行されていない", () => {
+    const mockFn = jest.fn();
+    expect(mockFn).not.toBeCalled();
+})
+
+test("モック関数は実行された回数を記録している", () => {
+    const mockFn = jest.fn();
+    mockFn();
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    mockFn();
+    expect(mockFn).toHaveBeenCalledTimes(2);
+})
+
+test("モック関数は関数の中でも実行できる", () => {
+    const mockFn =jest.fn();
+    function greet() {
+        mockFn();
+    }
+    greet();
+    expect(mockFn).toHaveBeenCalledTimes(1);
+});
+
+test("モック関数は実行時の引数を記録している", () => {
+    const mockFn = jest.fn();
+    function greet(message: string) {
+        mockFn(message);
+    }
+    greet("hello");
+    expect(mockFn).toHaveBeenCalledWith("hello")
+})
+
+test("モック関数はテスト対象の引数として使用できる", () => {
+    const mockFn = jest.fn();
+    greet("Jiro", mockFn);
+    expect(mockFn).toHaveBeenCalledWith("Hello! Jiro");
+})
+
+test("モック関数は実行時引数のオブジェクト検証ができる", () => {
+    const mockFn = jest.fn();
+    checkConfig(mockFn);
+    expect(mockFn).toHaveBeenCalledWith({
+        mock: true,
+        feature: { spy: true },
+    })
+})
+
+test("expect.objectContainingによる部分検証", () => {
+    const mockFn = jest.fn();
+    checkConfig(mockFn);
+    expect(mockFn).toHaveBeenCalledWith (
+        expect.objectContaining({
+            feature: { spy: true },
+        })
+    )
+})
